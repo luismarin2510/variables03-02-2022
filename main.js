ed7e093f0c0e6e8207329a8ebe48681e49377283
luismarin2510 <opraedu@.com>
2022-02-06 18:11:11 -0500

    mensajes de consola

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ document.querySelector("#abrir").addEventListener("click",(e)=>{
 document.querySelector("#botonCerrar").addEventListener("click",(e)=>{
     document.querySelector("#ventana").close();
 })
+console.log("mensaje")
+console.warn("ten cuidado");
+console.error("mortal");
+console.group ("mensajes agrupados");
+let datos = [
+    {nombre:"luis",edad:30},
+    {nombre:"pepito",edad:20}
+
+];
+console.table(datos);
+
+console.groupEnd();
+
+console.log("%c personalizado","color:green; font-size:15px");
